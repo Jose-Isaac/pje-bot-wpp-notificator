28fdc263b92bb907af720b015928798a0b1719c0
Jose Isaac <joseisaacbarreirocampos@gmail.com>
2023-10-02 13:42:53 -0300

    add email connect

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const { app } = require("./config")
-const { nodemailer } = require("nodemailer")
 const { wppCreateConnection } = require("./services/wppService")
+const { emailConnect } = require("./services/emailService")
 
 const serverPort = process.env.SERVER_PORT || 3001
 
 wppCreateConnection()
+emailConnect()
 
 app.listen(serverPort, () => {
     console.log(`Server listening in ${serverPort}`);
